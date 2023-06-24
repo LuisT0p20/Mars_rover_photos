656973bee7495209b3e4e182288f93782b25b464
LuisT0p20 <luistoribiopalacios@gmail.com>
2023-06-24 13:01:55 -0500

    funcionalidad_login con firebase

--- app/src/main/java/com/ltp/mars_rover_photos/login.java
+++ app/src/main/java/com/ltp/mars_rover_photos/login.java
@@ -1,14 +1,79 @@
 package com.ltp.mars_rover_photos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
-
+    EditText edtEmail,edtPassword;
+    Button btnSignIn,btnSignUp;
+    AwesomeValidation awesomeValidation;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        edtEmail = findViewById(R.id.edtEmailIn);
+        edtPassword = findViewById(R.id.edtPasswordIn);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        //Obtener instancia de firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseAuth mAuth =FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null){
+            iraPrincipal();
+        }
+
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        awesomeValidation.addValidation(this,R.id.edtEmailIn, Patterns.EMAIL_ADDRESS,R.string.validation_email);
+        awesomeValidation.addValidation(this,R.id.edtPasswordIn, ".{6,}",R.string.validation_password);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (awesomeValidation.validate()){
+                    String email = edtEmail.getText().toString();
+                    String password = edtPassword.getText().toString();
+
+                    firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                iraPrincipal();
+
+                            }else {
+                                String error = ((FirebaseAuthException) task.getException()).getErrorCode();
+                                Toast.makeText(login.this, error, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void iraPrincipal() {
+        Intent intent = new Intent(this, menu.class);
+        intent.putExtra("email",edtEmail.getText().toString());
+        startActivity(intent);
     }
 }
