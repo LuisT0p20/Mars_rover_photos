8b04a874d1d58cdc57ea436b6686604915af3a88
SanchezEirric <74950043@continental.edu.pe>
2023-06-25 18:43:43 -0500

    Avance 25-Jun

---
+++ app/src/main/java/com/ltp/mars_rover_photos/Register.java
@@ -0,0 +1,16 @@
+package com.ltp.mars_rover_photos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ltp.mars_rover_photos.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
